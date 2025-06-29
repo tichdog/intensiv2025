7e6380e1ade92d28b11294b00086e3ce60215349
Artem <artiom.kopyow@yandex.ru>
2025-06-29 13:20:29 +0300

    Информация о легендах берется из json файла, добавлены изображения

---
+++ AndroidStudioProj/app/src/main/java/com/example/intensiv/PointsData.java
@@ -0,0 +1,11 @@
+package com.example.intensiv;
+
+import java.util.List;
+
+class PointsData {
+    private List<PointData> points;
+
+    public List<PointData> getPoints() {
+        return points;
+    }
+}
