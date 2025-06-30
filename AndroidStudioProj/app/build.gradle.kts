21effee1e521a2739cbe83750bd4bb28d08947a0
KulikovE <zh.culikov2014@yandex.ru>
2025-06-30 20:06:04 +0300

    Построение маршрута для всех типов транспорта

--- AndroidStudioProj/app/build.gradle.kts
+++ AndroidStudioProj/app/build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("org.osmdroid:osmdroid-android:6.1.16")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
-    implementation("com.yandex.android:maps.mobile:4.16.0-full")
+    implementation("com.yandex.android:maps.mobile:4.4.0-full")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
